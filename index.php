3be40a1128794f8dcd9adc9634b1e8bb07d6b68f
Vitaliy Moskalenko <to-@mail.ru>
2020-03-21 20:38:20 +0300

    Components added

--- index.php
+++ index.php
@@ -1,50 +1,41 @@
 <!DOCTYPE html>
 <html>	
-	<head>
-		<meta charset="utf-8">
-		<title>VUE2 and SPRING5</title>
-		
-		<link rel="stylesheet" type="text/css" href="css/main.css" />		
-	</head>
-	<body>
+<head>
+	<meta charset="utf-8">
+	<title>VUE2 and SPRING5</title>
 	
-	  <div class="main">
-		<div id="app">
+	<link rel="shortcut icon" href="img/icons/favicon.ico" type="image/x-icon">		
+	<link rel="stylesheet" type="text/css" href="css/main.css" />		
+</head>
+<body>
+	
+	<div class="main">
+		<div id="app" v-cloak>
+			<!-- Vue stuff  : is short for v-bind  @ - v-on  -->
 		
-			<ul>
+			<!-- Define 'MessageList' Vue component -->
+			<message-list v-bind:items="messages" @on-delete="deleteMessage"></message-list>
+			<!-- <ul>
 				<li v-for="msg in messages">
-					{{ msg.text }} - {{ msg.createdAt }}				
+					{{ msg.text }} - {{ msg.createdAt }}
+					<button @click="deleteMessage">X</button>	
 				</li>
-			</ul>
+			</ul> -->
+			
 			<form v-on:submit.prevent="addMessage">
-				<textarea v-model="newMessage"
-					placeholder="Leave a message"				
-				></textarea>
-			
-			
-			</form>
-		
-		
-		
-		
+				<textarea v-model="newMessage" placeholder="Leave a message"></textarea>
+				<div>	
+					<button type="submit" v-bind:disabled="disableNewMessages">Add</button>
+				</div>		
+			</form>		
 
 		</div>		  
-	  </div>
+	</div>
 		
-	  <footer><a href="/learnVue">Home</a></footer>	
+	<footer><a href="/learnVue">Home</a></footer>	
 	
-<script type="text/javascript" src="js/vue2-6-11.js"></script>
-<script type="text/javascript">
-	let viewModel = new Vue({
-		el:   '#app',
-		data: {
-			messages:   [],
-			newMessage: ''
-		}
-	});
-
-
-</script>
+<script type="text/javascript" src="vendor/vue2-6-11.js"></script>
+<script type="module" src="js/main.js"></script>
 	
-	</body>
+</body>
 </html>	
